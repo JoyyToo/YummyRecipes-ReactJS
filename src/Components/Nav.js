f52a8c56174865f0c75f95027027a7fd80ab43b5
joyytoo <joytoo97@gmail.com>
2018-02-12 11:08:46 +0300

    Update UI

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import {notify} from 'react-notify-toast'
 import './Landing.css'
 
-const nav = () => {
-    return (
-        <div className="container">
-        <Link to="/" className="navbar-brand" style={{ textDecoration: 'none' }}><h2>YUMMY</h2></Link>
-        </div>
-    )
-};
+class Nav extends Component {
 
-export default nav;
+    handleLogout = (event) => {
+        window.localStorage.clear()
+        notify.show('You logged out successfully.', 'success', 4000)
+    }
+
+    render() {
+        const Accesstoken = window.localStorage.token
+
+        return (
+            
+            <div className="container">
+            <Link to="/" className="navbar-brand" style={{ textDecoration: 'none' }}><h2>YUMMY
+
+            { !Accesstoken ? (
+                        ''
+                    ) : (
+                        <Link to="/" className="navbar-brand" style={{ float: 'right', marginRight: 40, textDecoration: 'none' }}
+                        onClick={(event => this.handleLogout(event))} >LOGOUT</Link>
+                    )}
+
+            <a className="navbar-brand" style={{ float: 'right',  marginRight: 20, color: 'white'}}>           
+                Hello
+                { !Accesstoken ? (
+                    ' Guest'
+                ) : (
+                    ' ERER'//IF TOKEN
+                )}
+            </a>
+            </h2>
+            </Link>
+            
+            </div>
+        )
+    }}
+
+    export default Nav;
