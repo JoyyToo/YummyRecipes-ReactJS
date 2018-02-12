2106a28a53ee6015ab14677784388aba7ca5ec2f
joyytoo <joytoo97@gmail.com>
2018-02-12 11:09:23 +0300

    Update app routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Notification from 'react-notify-toast'
 import './App.css';
 import Navbar from './Components/Nav'
 import Landing from './Components/Landing'
 import Register from './Components/Auth/Register'
 import Login from './Components/Auth/Login'
+import ResetPassword from './Components/Auth/ResetPass'
+import NewPassword from './Components/Auth/NewPassword'
 import AddCategory from './Components/Categories/Addcategory'
 import Categories from './Components/Categories/Viewcategories'
 import AddRecipe from './Components/Recipes/Addrecipe'
+import Recipe from './Components/Recipes/Viewrecipes'
 import UpdateCategory from './Components/Categories/Updatecategory'
 import UpdateRecipe from './Components/Recipes/Updaterecipe'
 
@@ -18,21 +22,20 @@ class App extends Component {
       <BrowserRouter>
       <div>
         <Navbar />
-
+        <Notification />
         <Switch>
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/resetpassword" component={ResetPassword} />
+          <Route exact path="/newpassword" component={NewPassword} />
           <Route exact path="/categories" component={Categories} />
           <Route exact path="/addcategory" component={AddCategory} />
           <Route exact path="/updatecategory" component={UpdateCategory} />
-          <Route exact path="/addrecipe" component={AddRecipe} />
+          <Route exact path="/recipes" component={Recipe} />
           <Route exact path="/addrecipe" component={AddRecipe} />
           <Route exact path="/updaterecipe" component={UpdateRecipe} />
         </Switch>
-        <div className="copyright">
-            <p>Copyright &copy; Yummy Recipes 2018. All Rights Reserved</p>
-        </div>
       </div>
       </BrowserRouter>
     );
