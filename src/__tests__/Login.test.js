8bdfca8868732c2f85534dce27dc74f1e23b26a8
joyytoo <joytoo97@gmail.com>
2018-03-08 10:16:49 +0300

    [chore] Add tests

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -96,40 +96,50 @@ describe('Login component', () => {
         expect(wrapper.find('div')).toHaveLength(4);
     });
 
-    it('renders component in li', () => {
+    it('contains li', () => {
         expect(wrapper.find('li')).toHaveLength(2);
     });
 
-    it('renders component in p', () => {
+    it('contains p', () => {
         expect(wrapper.find('p')).toHaveLength(2);
     });
 
-    it('renders component in br', () => {
+    it('contains br', () => {
         expect(wrapper.find('br')).toHaveLength(8);
     });
 
-    it('renders component in MuiThemeProvider', () => {
+    it('contains in MuiThemeProvider', () => {
         expect(wrapper.find('MuiThemeProvider')).toHaveLength(1);
     });
 
-    it('renders component in Link', () => {
+    it('contains Link', () => {
         expect(wrapper.find('Link')).toHaveLength(4);
     });
 
-    it('renders component in Paper', () => {
+    it('contains Paper', () => {
         expect(wrapper.find('Paper')).toHaveLength(1);
     });
 
-    it('renders component in i', () => {
+    it('contains i', () => {
         expect(wrapper.find('i')).toHaveLength(2);
     });
 
-    it('renders component in TextField ', () => {
+    it('contains TextField ', () => {
         expect(wrapper.find('TextField')).toHaveLength(3);
     });
 
-    it('renders component in button', () => {
+    it('contains button', () => {
         expect(wrapper.find('button')).toHaveLength(3);
     });
 
+    it('has initial state', () => {
+        expect(wrapper.state().email).toEqual('');
+        expect(wrapper.state().password).toEqual('');
+    });
+
+    it('has the correct form fields', () => {
+        expect(wrapper.find('[name="email"]')).toHaveLength(2);
+        expect(wrapper.find('[name="password"]')).toHaveLength(1);
+    });
+    
 })
