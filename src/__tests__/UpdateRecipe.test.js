2c04c34ac3513d710caabc20dbfe24ac911bf691
joyytoo <joytoo97@gmail.com>
2018-03-10 14:30:32 +0300

    [chore] Refactor tests

--- src/__tests__/UpdateRecipe.test.js
+++ src/__tests__/UpdateRecipe.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 
-import UpdateRecipe from '../Components/Recipes/Updaterecipe'
+import UpdateRecipe from '../Components/Recipes/UpdateRecipe'
 
 describe('UpdateRecipe  component', () => {
     const params = {
