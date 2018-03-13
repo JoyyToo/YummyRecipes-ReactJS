3daad2839c53544a7fb31cb2e30d12ce04b98b32
joyytoo <joytoo97@gmail.com>
2018-03-13 11:24:25 +0300

    [chore] Refactor code

--- src/Components/Recipes/Recipe.js
+++ src/Components/Recipes/Recipe.js
@@ -131,150 +131,86 @@ class Recipe extends Component {
       marginLeft: 20,
     };
 
-    // handle get recipe request
-    handleRecipe = (page=1) => {
-        const category_id = this.props.match.params['category_id'];
-    }
-    // mount token when page loads
-    componentDidMount() {
-        this.setState({
-            token: window.sessionStorage.accessToken,
-        });
-        const token = window.localStorage.getItem('token');
-        if (!token) {
-            window.location.replace('/login')
-        }
-
-        // send GET request to API
-        axiosInstance.get(`category/${category_id}/recipes?page=${page}`)
-            .then((response) => {
-                this.setState({
-                    data: response.data,
-                    recipes: response.data[1],
-                    id: this.state.id,
-                    category_id: this.state.category_id,
-                    pagination: response.data[0]
-                })
-            })
-
-            .catch((error) => {
-                if (error.response){
-                    notify.show(error.response.data.message,'error', 4000);
-                }
-            });
-    };
+    const styles = {
+      card2: {
+        position: 'relative',
+        padding: 10,
+        width: 450,
+        float: 'left',
+        margin: 5,
 
       },
       align: {
         align: 'left',
-      
 
-    // handle delete recipe request
-    handleDeleteRecipe = (event) => {
-        let id = event.currentTarget.getAttribute('id');
-        const category_id = this.props.match.params['category_id'];
-
-        // send DELETE request to API
-        axiosInstance.delete(`category/${category_id}/recipes/${id}`)
-            .then((response) => {
-                this.handlerecipe();
-
-        const token = window.localStorage.getItem('token');
-        // send DELETE request to API
-        axiosInstance.delete(`category/${category_id}/recipes/${id}`)
-            .then((response) => {
-                this.handlerecipe();
-                notify.show(response.data.message, 'success', 4000);
-                this.setState({
-                    id: this.state.id
-                });
-                this.props
-                    .history
-                    .push(`/categories/${category_id}/recipes`);
-            })
-            .catch((error) => {
-                notify.show(error.response, 'error', 4000);
-            });
-    });
-
-
-    handleSearch= (event) => {
-        const category_id = this.props.match.params['category_id'];
-
-        // send GET request to API
-        axiosInstance.get(`category/${category_id}/recipes?q=${this.state.q}`)
-            .then((response) => {
-                let recipe = response.data['recipes'];
-                this.setState({
-                        q: "",
-                        recipes: recipe,
-                });   
-            })
-            .catch((error) => {
-                notify.show(error.response.data.message, 'error', 4000);
-            });
+      },
     };
 
-        // send GET request to API
-        axiosInstance.get(`category/${category_id}/recipes?q=${this.state.q}`)
-            .then((response) => {
-                let recipe = response.data['recipes'];
-                this.setState({
-                        q: "",
-                        recipes: recipe,
-                });   
-            })
-            .catch((error) => {
-                notify.show(error.response.data.message, 'error', 4000);
-            });
-    };
+    const categoryid = this.props.match.params['category_id'];
+    return (
+      <div>
 
-    render() {
-        let recipe = this.state.recipes;
+        <MuiThemeProvider>
+          <div>
+            <Link to={`/categories/${categoryid}/addrecipe`}>
+              <FloatingActionButton secondary={true} style={style}>
+                <ContentAdd />
+              </FloatingActionButton>
+            </Link>
+            <AutoComplete
+              hintText="Search"
+              searchText={this.state.q}
+              onUpdateInput={this.handleInputChange}
+              onNewRequest={this.handleSearch.bind(this)}
+              dataSource={this.state.recipes}
+              filter={(searchText, key) => (key.indexOf(searchText) !== -1)}
+              openOnFocus={true}
+              style={{
+                margin: '0 auto',
+                width: 430,
+                float: 'right',
+                marginRight: 75,
+                backgroundColor: 'white',
+                paddingLeft: 20,
+              }}
+            />
+          </div>
 
-        let style = {
-            marginLeft: 20,
-        };
+          <div>
 
-        const styles = {
-            card2: {
-                position: 'relative',
-                padding: 10,
-                width: 450,
-                float: 'left',
-                margin: 5,
+            {
 
-            },
-            align: {
-                align: 'left'
+              Object.keys(recipe).map(key => (
+                <div style={styles.card2} key={key}>
+                  <MuiThemeProvider is="nospace start">
+                    <Card style={{ height: 200 }}>
+                      <h2 style={{
+                        marginLeft: 30,
+                        paddingTop: 10,
+                      }}
+                      >
+                        {recipe[key]['name']}
+                      </h2>
+                      <p style={{ marginLeft: 30 }}>Time: {recipe[key]['time']}</p>
+                      <CardText>
+                        {
 
-            }
-        };
+                        }
+                      </CardText>
 
-        const category_id = this.props.match.params['category_id'];
-        return (
-            <div>
-                
-                <MuiThemeProvider>
-                    <div>
-                        <Link to={"/categories/"+ category_id +"/addrecipe"}>
-                            <FloatingActionButton secondary={true} style={style}>
-                                <ContentAdd/>
-                            </FloatingActionButton>
-                        </Link>
-
-                        <SearchBar name="q" value={this.state.q}
-                            onChange={this.handleInputChange}
-                            onRequestSearch={this.handleSearch.bind(this)}
-
-                            style={{
-                            margin: '0 auto',
-                            width: 430,
-                            float: 'right',
-                            marginRight: 75,
-                            backgroundColor: 'white',
-                            paddingLeft: 20                  
-                            }}
+                      <Link to={`/categories/${recipe[key]['category_id']}/recipe/${recipe[key]['id']}`}>
+                        <FlatButton
+                          label="View"
+                          category_id={recipe[key]['category_id']}
+                          id={recipe[key]['id']}
+                        />
+                      </Link>
+                      <Link to={`/categories/${recipe[key]['category_id']}/recipes/${recipe[key]['id']}`}>
+                        <FlatButton
+                          label="Edit"
+                          id={recipe[key]['id']}
+                          desc={recipe[key]['desc']}
+                          primary={true}
                         />
                       </Link>
                       <FlatButton
